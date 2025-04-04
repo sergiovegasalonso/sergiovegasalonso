6d2eb179011b6ce1c1455403c40ae5fd92f5db0d
Sergio Vegas Alonso <sergio.vegas.alonso@gmail.com>
2025-04-04 21:17:25 +0200

    ok

--- src/pages/api/download-cv.ts
+++ src/pages/api/download-cv.ts
@@ -1,23 +1,19 @@
-import { promises as fs } from 'fs';
-import path from 'path';
+import { promises as fs } from "fs";
+import path from "path";
 
 export async function GET() {
   try {
-    // Resolve the path to the PDF file
-    const filePath = path.resolve('./src/pages/api', 'sample.pdf');
-    
-    // Read the PDF file
+    const filePath = path.resolve("./src/pages/api", "sample.pdf");
     const fileBuffer = await fs.readFile(filePath);
 
-    // Return the PDF file as a response
     return new Response(fileBuffer, {
       headers: {
-        'Content-Type': 'application/pdf',
-        'Content-Disposition': 'attachment; filename="sample.pdf"',
+        "Content-Type": "application/pdf",
+        "Content-Disposition": 'attachment; filename="sample.pdf"',
       },
     });
   } catch (error) {
-    console.error('Error reading the PDF file:', error);
-    return new Response('Failed to download the file', { status: 500 });
+    console.error("Error reading the PDF file:", error);
+    return new Response("Failed to download the file", { status: 500 });
   }
 }
