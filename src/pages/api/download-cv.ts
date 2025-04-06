01e86089c609e6182981c426e1e02b8a6367642e
Sergio Vegas Alonso <sergio.vegas.alonso@gmail.com>
2025-04-06 12:43:15 +0200

    ok

--- src/pages/api/download-cv.ts
+++ src/pages/api/download-cv.ts
@@ -3,13 +3,17 @@ import path from "path";
 
 export async function GET() {
   try {
-    const filePath = path.resolve("./src/pages/api", "sample.pdf");
+    const filePath = path.resolve(
+      "./src/pages/api",
+      "SergioVegasAlonso-CV.pdf",
+    );
     const fileBuffer = await fs.readFile(filePath);
 
     return new Response(fileBuffer, {
       headers: {
         "Content-Type": "application/pdf",
-        "Content-Disposition": 'attachment; filename="sample.pdf"',
+        "Content-Disposition":
+          'attachment; filename="SergioVegasAlonso-CV.pdf"',
       },
     });
   } catch (error) {
