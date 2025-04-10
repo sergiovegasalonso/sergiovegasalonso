466b369fe0dd296eb2f922d2aa98f13dd26d31c7
Sergio Vegas Alonso <sergio.vegas.alonso@gmail.com>
2025-04-10 17:24:39 +0200

    sitemap and compression configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,14 @@ import { defineConfig } from "astro/config";
 
 import tailwindcss from "@tailwindcss/vite";
 
+import sitemap from "@astrojs/sitemap";
+
+import playformCompress from "@playform/compress";
+
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+  site:"https://sergiovegasalonso.vercel.app/",
+  integrations: [sitemap(), playformCompress()],
 });
